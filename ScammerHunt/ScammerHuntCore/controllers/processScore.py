dcd674b3f41231e70f8b63bb394638c0c322cd22
Shweta Sawant <shweta@epifi.com>
2022-12-03 18:11:28 +0530

    Update processScore.py
    Added score function.

--- ScammerHunt/ScammerHuntCore/controllers/processScore.py
+++ ScammerHunt/ScammerHuntCore/controllers/processScore.py
@@ -1,3 +1,22 @@
-def get_score(has_mentioned_users, keywords_count, duplicate_count, retweets_count, like_count):
-    # TODO (shwetha)
-    pass
+
+keywords = ['','']
+
+def get_score(mentioned_users, keywords, tweet_ids, retweets_count, like_count):
+    score = 0
+
+    if len(keywords) > 0:
+        score +=5
+    if len(mentioned_users) > 0:
+        score += 2
+    if len(tweet_ids) > 0:
+        score += 1
+    if retweets_count > 0:
+        score += 1
+    elif like_count > 0:
+        score += 1
+    return score
+
+score = get_score(mentioned_users=[], keywords=keywords, tweet_ids=[], retweets_count=2, like_count=3)
+print(score)
+
+#python3 ScammerHunt/ScammerHuntCore/controllers/processScore.py
