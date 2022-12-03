bfb40e364bc4482d2c38c200878c06ce7b6a22da
Surya gangaraj <surya.g@epifi.com>
2022-12-04 04:23:38 +0530

    Revert "Merge branch 'main' of https://github.com/surya-epifi/Hackathon-Dec-22"
    This reverts commit 1a1a981026fd8800ae9c2a07945143be6acd88c6, reversing
    changes made to 06d6b8f547ac79294fbae946b16ecf0cc895a3b4.

--- ScammerHunt/ScammerHuntCore/controllers/processScore.py
+++ ScammerHunt/ScammerHuntCore/controllers/processScore.py
@@ -1,42 +1,19 @@
 
-KEYWORDSTOCHECKFROM = ['scam','fraud', 'money', 'bank', 'criminal', 'debited', 'extorsion', 'balance']
-USERSTAGGEDTOCHECKFROM = ['@police','@dgim', '@delhipolice', '@mhPolice', '@goaPolice', '@cybercrime', '@dgp']
-# TODO: Create weight array for each of the keywords and use that to calculate the score
-
-SCOREWEIGHTAGEFORKEYWORDS = 5
-SCOREWEIGHTAGEFORUSERSTAGGED = 3
-SCOREWEIGHTAGEFORREPEATEDTWEETIDS = 1
-SCOREWEIGHTAGEFOROTHER = 1
+keywords = ['','']
 
 def get_score(mentioned_users, keywords, tweet_ids, reply_count, retweets_count, like_count):
     score = 0
 
     if len(keywords) > 0:
-        score += getOccuranceScoreFor(elementTypeArray=keywords, referenceArray=KEYWORDSTOCHECKFROM, scoreWeightage=SCOREWEIGHTAGEFORKEYWORDS)
+        score +=5
     if len(mentioned_users) > 0:
-        score += getOccuranceScoreFor(elementTypeArray=mentioned_users, referenceArray=USERSTAGGEDTOCHECKFROM, scoreWeightage=SCOREWEIGHTAGEFORUSERSTAGGED)
+        score += 2
     if len(tweet_ids) > 0:
-        score += getTweetIdScore(tweet_ids=tweet_ids)
-    if retweets_count > 0 or like_count > 0:
-        score += SCOREWEIGHTAGEFOROTHER
+        score += 1
+    if retweets_count > 0:
+        score += 1
+    elif like_count > 0:
+        score += 1
     return score
-
-def getTweetIdScore(tweet_ids):
-    if tweet_ids > 0:
-        return SCOREWEIGHTAGEFORREPEATEDTWEETIDS
-
-def getOccuranceScoreFor(elementTypeArray, referenceArray, scoreWeightage):
-
-    listWithNoDuplicates = [*set(elementTypeArray)]
-    commonKeywords = 0
-    for word in listWithNoDuplicates:
-        if word in referenceArray:
-            commonKeywords += 1
-    
-    score = (commonKeywords/len(KEYWORDSTOCHECKFROM))*scoreWeightage
-    return score
-
-score = get_score(mentioned_users=['@police'], keywords=['scam','fraud', 'money', 'bank','scam','fraud', 'money', 'bank'], tweet_ids=[], retweets_count=2, like_count=2)
-print(score)
 
 #python3 ScammerHunt/ScammerHuntCore/controllers/processScore.py
