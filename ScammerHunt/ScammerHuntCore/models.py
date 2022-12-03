8507c6c934c0a56bccebbef998b601f3b8dd78ee
Surya gangaraj <surya.g@epifi.com>
2022-12-03 21:45:49 +0530

    Update

--- ScammerHunt/ScammerHuntCore/models.py
+++ ScammerHunt/ScammerHuntCore/models.py
@@ -4,6 +4,12 @@ SOURCE_TYPE = (
 
 from django.db import models
 
+class Keywords(models.Model):
+    name = models.CharField(max_length=300, default='')
+
+class PriorityUser(models.Model):
+    username = models.CharField(max_length=300, default='')
+
 # Mainly tweets
 class ScrapeData(models.Model):
     text_data = models.TextField()
@@ -13,17 +19,11 @@ class ScrapeData(models.Model):
     reply_count = models.IntegerField(default=0)
     retweet_count = models.IntegerField(default=0)
     like_count = models.IntegerField(default=0)
-    # keywords = models.ManyToManyField("Keywords")
-    # mentioned_users = models.ManyToManyField("PriorityUser", related_name="data_mentioned_users")
+    keywords = models.ManyToManyField(Keywords)
+    mentioned_users = models.ManyToManyField(PriorityUser, related_name="data_mentioned_users")
     source = models.CharField(default='TWITTER', choices=SOURCE_TYPE, max_length=50)
 
 class Scammer(models.Model):
     name = models.CharField(max_length=300, default='')
     phone_number = models.CharField(max_length=300, primary_key=True)
     email = models.CharField(max_length=300, default='')
-
-class Keywords(models.Model):
-    name = models.CharField(max_length=300, default='')
-
-class PriorityUser(models.Model):
-    username = models.CharField(max_length=300, default='')
