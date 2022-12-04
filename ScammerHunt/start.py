eacbaa2b493595afdfde5741539993088a3e3de6
Surya gangaraj <surya.g@epifi.com>
2022-12-04 13:53:52 +0530

    Util update

--- ScammerHunt/start.py
+++ ScammerHunt/start.py
@@ -26,6 +26,8 @@ def is_scam_related_tweet(tweet, phone_numbers):
         return False
     
 
+# clearAllData()
+
 # TODO: include (place:b850c1bfd38f30e0)
 # Using TwitterSearchScraper to scrape data and append tweets to list
 for i,tweet in enumerate(sntwitter.TwitterSearchScraper('(phone OR number OR mobile OR no OR bank )(fraudster OR scammer OR cheat OR scam OR fraud OR loot ) (@CPMumbaiPolice  OR @cyberdost OR @noidapolice OR @uppolice OR @delhipolice OR @cybercrimeCID OR @mumbaipolice OR @dgpMaharashtra OR @mahaPolice OR @blrCityPolice OR @cybercellRaj OR @cybercellindia OR @DoT_India  OR @assampolice OR @wbpolice OR @kolkatapolice) (since:2022-11-05 until:2022-12-01)').get_items()):        
